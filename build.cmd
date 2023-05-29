925560a6dde6fd2ae0076a4fbd07ccc2aea1ffed
Daniel Jackson <email@danieljackson.co.uk>
2023-05-29 21:22:47 +0100

    -

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
-CD /D %~dp0
+PUSHD %~dp0
 
 rem If launched from anything other than cmd.exe, will have "%WINDIR%\system32\cmd.exe" in the command line
 ECHO.%CMDCMDLINE% | FINDSTR /C:"%COMSPEC% /c" >NUL
@@ -25,7 +25,7 @@ ECHO Build tools not on path, looking for 'vcvarsall.bat'...
 SET ARCH=x86
 SET VCVARSALL=
 FOR %%f IN (70 71 80 90 100 110 120 130 140) DO IF EXIST "!VS%%fCOMNTOOLS!\..\..\VC\vcvarsall.bat" SET VCVARSALL=!VS%%fCOMNTOOLS!\..\..\VC\vcvarsall.bat
-FOR /F "usebackq tokens=*" %%f IN (`DIR /B /ON "%ProgramFiles(x86)%\Microsoft Visual Studio\????"`) DO FOR %%g IN (Community Professional Enterprise) DO IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\%%f\%%g\VC\Auxiliary\Build\vcvarsall.bat" SET "VCVARSALL=%ProgramFiles(x86)%\Microsoft Visual Studio\%%f\%%g\VC\Auxiliary\Build\vcvarsall.bat"
+FOR /F "usebackq tokens=*" %%f IN (`DIR /B /ON "%ProgramFiles(x86)%\Microsoft Visual Studio\????"`) DO FOR %%g IN (BuildTools Community Professional Enterprise) DO IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\%%f\%%g\VC\Auxiliary\Build\vcvarsall.bat" SET "VCVARSALL=%ProgramFiles(x86)%\Microsoft Visual Studio\%%f\%%g\VC\Auxiliary\Build\vcvarsall.bat"
 IF "%VCVARSALL%"=="" ECHO Cannot find C compiler environment for 'vcvarsall.bat'. & GOTO ERROR
 ECHO Setting environment variables for C compiler... %VCVARSALL%
 CALL "%VCVARSALL%" %ARCH%
@@ -50,4 +50,7 @@ GOTO :EOF
 ECHO ERROR: An error occured.
 IF DEFINED INTERACTIVE COLOR 4F & PAUSE & COLOR
 EXIT /B 1
-GOTO :EOF
+GOTO END
+
+:END
+POPD
